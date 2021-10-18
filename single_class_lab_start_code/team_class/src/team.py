35868ae39edc83bdc4180504beed4b5503905325
Heather King <hkingdevelopment@gmail.com>
2021-10-18 15:07:46 +0100

    "final commit with all tests passed"

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -3,6 +3,29 @@ class Team:
         self.name = team_name
         self.players = players_names
         self.coach = coach_name
+        self.points = 0
 
     def add_player(self, new_player):
         self.players.append(new_player)
+
+    def has_player(self, player_name):
+        # if self.players.count(player_name) > 0:
+        #     return True
+        # else:
+        #     return False
+
+        if player_name in self.players:
+            return True
+        else:
+            return False
+
+        
+        
+    def play_game(self, has_won):
+        if has_won == True:
+            self.points = 3
+        else:
+            self.points = 0
+        
+
+    
