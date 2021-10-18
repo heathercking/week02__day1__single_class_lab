fccf2a797e03eb91d7fa9697e084d92455f41b5c
Heather King <hkingdevelopment@gmail.com>
2021-10-18 14:33:11 +0100

    adds add_player function to team exercise

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -1,3 +1,8 @@
 class Team:
-    def __init__(self, name, players, coach):
-        
+    def __init__(self, team_name, players_names, coach_name):
+        self.name = team_name
+        self.players = players_names
+        self.coach = coach_name
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
