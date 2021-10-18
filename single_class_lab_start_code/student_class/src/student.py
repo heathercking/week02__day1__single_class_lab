8df567fd30cecbaa79349a42f0fd365076817541
Heather King <hkingdevelopment@gmail.com>
2021-10-18 14:23:41 +0100

    student class complete

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,4 +1,10 @@
 class Student:
     def __init__(self, student_name, cohort_number):
-           self.name = student_name
-           self.cohort = cohort_number
+        self.name = student_name
+        self.cohort = cohort_number
+
+    def talk(self):
+        return "I can talk!"
+    
+    def say_favourite_language(self, language):
+        return f"I love {language}"
